173d7c35d1c8ebd09eca9e24697a55d16d9542ed
kisihara_c <kisihara.c@gmail.com>
2021-02-21 12:22:08 +0900

    js-yaml-loaderInstalled

--- pages/index.js
+++ pages/index.js
@@ -20,40 +20,47 @@ import { useColorMode,useColorModeValue } from "@chakra-ui/react";
 
 //サンプルデータ　後に削除
 let dbSample = [
-  {title: "ここだけ全員雛見沢症候群L5",about:"ひぐらしファンの集いです" ,
+  {title: "動作テスト：ここだけ全員雛見沢症候群L5",about:"ひぐらしファンの集いです" ,
   dotw: "月",start:"20:00",end:"21:00",howto:"主催にjoin",other:"",
   link:"",
-  editableMessage:"やりますよ"},
+  editableMessage:"やりますよ",
+  password: "l5pass"},
       
   {title: "バー無言",about:"全員マイクを切らなくてはいけないバーです。ペンがあるのでみんなでお絵描きする事も" ,
   dotw: "火",start:"23:30",end:"28:00",howto:"",other:"",
   link:"",
-  editableMessage:""},
+  editableMessage:"",
+  password: "silentpass"},
       
   {title: "絶対に笑ってはいけないVRChat1時",about:"交代でコント役をやり、笑った人間の絵を皆で一枚書くイベントです" ,
   dotw: "土",start:"23:00",end:"00:00",howto:"主催にjoin",other:"",
   link:"",
-  editableMessage:"すいません今週コント役のxxxです、誰か代わってくれませんか、尻が痛くて動けません"},
+  editableMessage:"すいません今週コント役のxxxです、誰か代わってくれませんか、尻が痛くて動けません",
+  password: "24pass"},
       
   {title: "お前を殺す",about:"ﾃﾞﾃﾞﾝ！　ﾃﾞﾃﾞｯﾃﾞ！（ﾁｬｶﾎﾟｺｷﾗﾘﾝ）　ﾃﾞﾃﾞﾝ！　ﾃﾞﾃﾞｯﾃﾞ！！！" ,
   dotw: "土",start:"22:00",end:"24:00",howto:"主催にjoin",other:"",
   link:"",
-  editableMessage:""},
+  editableMessage:"",
+  password: "killingpass"},
       
   {title: "こしぶつけっこ",about:"すごーい！　なにこれなにこれー！　あははー！　動けー！！！！" ,
   dotw: "土",start:"04:30",end:"7:00",howto:"こしぶつけっこォ",other:"",
   link:"",
-  editableMessage:"不知火舞がアウトでホムラ・ヒカリがセーフな理由→ hogehoge.fm"},
+  editableMessage:"不知火舞がアウトでホムラ・ヒカリがセーフな理由→ hogehoge.fm",
+  password: "kosibutukekkopass"},
       
   {title: "衛宮切嗣のモノマネイベント",about:"皆で集まって衛宮切嗣のモノマネをやる回です" ,
   dotw: "日",start:"22:00",end:"24:00",howto:"アヴァロン…",other:"ふざけるな！　ふざけるな！　バカヤロー！！",
   link:"",
-  editableMessage:"時計塔卒業できました！　聖杯作ったので、今なら本当に衛宮切嗣のモノマネができす！ｗ"},
+  editableMessage:"時計塔卒業できました！　聖杯作ったので、今なら本当に衛宮切嗣のモノマネができす！ｗ",
+  password: "timealterpass"},
       
   {title: "Toheartの来栖川先輩は魔女っ子可愛い",about:"Toheartの来栖川先輩は魔女っ子可愛い" ,
   dotw: "水",start:"22:00",end:"24:00",howto:"来栖川先輩にjoin",other:"Toheartの来栖川先輩は魔女っ子可愛い",
   link:"",
-  editableMessage:"Toheartの来栖川先輩は魔女っ子可愛い"},
+  editableMessage:"Toheartの来栖川先輩は魔女っ子可愛い",
+  password: "toheartpass"},
 ]
 
 
