90aee4e1a2f8e6851749e9e4a9f7f06c6556fcdd
kisihara_c <kisihara.c@gmail.com>
2021-02-22 00:14:14 +0900

    weekDesignWIP1

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ import { Grid, GridItem } from "@chakra-ui/react"
 import { Box } from "@chakra-ui/react"
 import { Container } from "@chakra-ui/react"
 import { Spinner } from "@chakra-ui/react"
+import { Text } from "@chakra-ui/react"
+import { Divider } from "@chakra-ui/react"
 
 import { ColorModeScript } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
@@ -19,52 +21,6 @@ const colorConfig = {
 }
 import { useColorMode,useColorModeValue } from "@chakra-ui/react";
 
-
-
-//サンプルデータ　後に削除
-let dbSample = [
-  {title: "動作テスト：ここだけ全員雛見沢症候群L5",about:"ひぐらしファンの集いです" ,
-  dotw: "月",start:"20:00",end:"21:00",howto:"主催にjoin",other:"",
-  link:"",
-  editableMessage:"やりますよ",
-  password: "l5pass"},
-      
-  {title: "バー無言",about:"全員マイクを切らなくてはいけないバーです。ペンがあるのでみんなでお絵描きする事も" ,
-  dotw: "火",start:"23:30",end:"28:00",howto:"",other:"",
-  link:"",
-  editableMessage:"",
-  password: "silentpass"},
-      
-  {title: "絶対に笑ってはいけないVRChat1時",about:"交代でコント役をやり、笑った人間の絵を皆で一枚書くイベントです" ,
-  dotw: "土",start:"23:00",end:"00:00",howto:"主催にjoin",other:"",
-  link:"",
-  editableMessage:"すいません今週コント役のxxxです、誰か代わってくれませんか、尻が痛くて動けません",
-  password: "24pass"},
-      
-  {title: "お前を殺す",about:"ﾃﾞﾃﾞﾝ！　ﾃﾞﾃﾞｯﾃﾞ！（ﾁｬｶﾎﾟｺｷﾗﾘﾝ）　ﾃﾞﾃﾞﾝ！　ﾃﾞﾃﾞｯﾃﾞ！！！" ,
-  dotw: "土",start:"22:00",end:"24:00",howto:"主催にjoin",other:"",
-  link:"",
-  editableMessage:"",
-  password: "killingpass"},
-      
-  {title: "こしぶつけっこ",about:"すごーい！　なにこれなにこれー！　あははー！　動けー！！！！" ,
-  dotw: "土",start:"04:30",end:"7:00",howto:"こしぶつけっこォ",other:"",
-  link:"",
-  editableMessage:"不知火舞がアウトでホムラ・ヒカリがセーフな理由→ hogehoge.fm",
-  password: "kosibutukekkopass"},
-      
-  {title: "衛宮切嗣のモノマネイベント",about:"皆で集まって衛宮切嗣のモノマネをやる回です" ,
-  dotw: "日",start:"22:00",end:"24:00",howto:"アヴァロン…",other:"ふざけるな！　ふざけるな！　バカヤロー！！",
-  link:"",
-  editableMessage:"時計塔卒業できました！　聖杯作ったので、今なら本当に衛宮切嗣のモノマネができす！ｗ",
-  password: "timealterpass"},
-      
-  {title: "Toheartの来栖川先輩は魔女っ子可愛い",about:"Toheartの来栖川先輩は魔女っ子可愛い" ,
-  dotw: "水",start:"22:00",end:"24:00",howto:"来栖川先輩にjoin",other:"Toheartの来栖川先輩は魔女っ子可愛い",
-  link:"",
-  editableMessage:"Toheartの来栖川先輩は魔女っ子可愛い",
-  password: "toheartpass"},
-]
 
 
 export default function Circle() {
@@ -88,7 +44,9 @@ export default function Circle() {
 
     //データ取得（全部）
     let [dbData,setDbData] = useState("");
-    let query = { query: "query MyQuery { events { title dotw } }" }
+    let query = 
+      { query: 
+        "query MyQuery { events { title about dotw start end howToJoin other link editableMessage password } }" }
 
     const fetchJSON = (q=query) => {
       fetch('https://vrcdaysdreams-hc.hasura.app/v1/graphql', {
@@ -222,8 +180,13 @@ export default function Circle() {
     function EventBoxInWeek(props){
       return(
         <Box my="1em">
-          event title：<br />
-          {props.eventInfo.title}<br />
+          <Divider marginBottom="0.5em"/>
+
+          {props.eventInfo.start} ～ {props.eventInfo.end}<br />
+          <Text fontSize="0.9em">{props.eventInfo.title}</Text>
+          <Text fontSize="0.6em" textAlign="right">{props.eventInfo.about}</Text>
+          <Text fontSize="0.8em">伝言：<br />{props.eventInfo.editableMessage}</Text>
+
         </Box>
       )
     }
@@ -231,8 +194,6 @@ export default function Circle() {
     
     
   }
-
-
 
 
   function Footer(){
